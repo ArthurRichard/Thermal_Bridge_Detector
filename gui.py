6b60a5bdab7463e5fea8ddda926c8d9e4735de59
arthrik <arthr.richard@gmail.com>
2015-02-24 15:36:50 +0100

    Passage a l'oriente objet

--- gui.py
+++ gui.py
@@ -1,39 +1,63 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
 __author__ = 'arthrik'
 
 from Tkinter import *
 
-TEMP_AMBIANTE = 25  #A remplacer par le registre I2C
 FOND_LCD = '#D3FFCE'
 
-mode = 1 #1: Image Reelle uniquement 2:Image infrarouge uniquement 3: Melange IR + RL
 
-fen = Tk()
-#fen.attributes('-fullscreen', 1)
-fen.geometry('1280x800')
+class Interface:
+    'Ici, c est la classe qui gere l interface'
 
-#imagefnl = PhotoImage(file="images/mix.gif")
-imagerl = PhotoImage(file="images/image.gif")
-imageir = PhotoImage(file="images/imageir.gif")
-echelle = PhotoImage(file="images/echelle.gif")
+    def __init__(self):
 
-canvas = Canvas(fen,width = 512,height = 256,relief=RAISED,bg=FOND_LCD)
-canvas.grid(row=0,column=1,sticky=N+E)
+        self.fen = Tk()
+        # fen.attributes('-fullscreen', 1)
+        self.fen.geometry('1280x800')
 
-canvas.create_text(200,100, font=("DS-Digital", 144),text=TEMP_AMBIANTE)
-canvas.create_text(350,100, font=("DS-Digital", 144),text="C")
-canvas.create_text(160,200, font=("DS-Digital", 32),text="Mode:")
-canvas.create_text(220,200, font=("DS-Digital", 32),text=mode)
+        # imagefnl = PhotoImage(file="images/mix.gif")
+        self.imagerl = PhotoImage(file="images/image.gif")  #Image venant de la webcam
+        self.imageir = PhotoImage(file="images/imageir.gif")  #Image infrarouge
+        self.degrade = PhotoImage(file="images/echelle.gif")  #Degrade de couleurs
 
-quitter = Button(fen, text="Quit", command=fen.destroy)
-quitter.grid(row=1,column=1)
+        self.canvas = Canvas(self.fen, width=512, height=256, relief=SUNKEN, bd=2, bg=FOND_LCD)  #Afficheur de temperature
+        self.canvas.grid(row=0, column=1, sticky=N + E)
 
-dispimage = Label(fen,image=imagerl)
-dispimage.grid(row=0,column=0,sticky=W+S+N)
+        self.canvastemp = Canvas(self.fen, width=256, height=512, bg='white')  #Echelle mouvante
+        self.canvastemp.grid(row=0, column=1, sticky=S + E)
 
-echelletemp = Label(fen,image=echelle)
-echelletemp.grid(row=0,column=1,sticky=W+S)
+        self.canvas.create_text(200, 100, font=("DS-Digital", 144), text=25)  #Affichage de la temperature ambiante
+        self.canvas.create_text(350, 100, font=("DS-Digital", 144), text="C")
+        self.canvas.create_text(160, 200, font=("DS-Digital", 32), text="Mode:")  #Affichage du mode selectionne
+        self.canvas.create_text(220, 200, font=("DS-Digital", 32), text=1)
 
-fen.mainloop()
+        self.quitter = Button(self.fen, text="Quit", command=self.fen.destroy)
+        self.quitter.grid(row=1, column=1)
+
+        self.dispimage = Label(self.fen, image=self.imagerl, relief=RAISED, bd=2)
+        self.dispimage.grid(row=0, column=0, sticky=W + S + N)
+
+        self.degradetemp = Label(self.fen, image=self.degrade)
+        self.degradetemp.grid(row=0, column=1, sticky=W + S)
+
+
+    def moreTemp(self, i):
+        Interface.temperature += i
+        return Interface.temperature
+
+
+    def getTemp(self):
+        return Interface.temperature
+
+
+    def run(self):
+        self.fen.mainloop()
+
+if __name__ == '__main__':
+    app = Interface()
+    app.run()
+
+
+
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Detecteur_de_ponts_thermiques',
+    version='0.2',
+    packages=['images'],
+    url='',
+    license='',
+    author='arthrik',
+    author_email='arthr.richard@gmail.com',
+    description='Un logiciel de traitement pour cameras thermiques'
+)
